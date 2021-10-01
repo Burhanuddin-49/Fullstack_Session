ce096059bb695861512a720aa8b1243abfe554ca
Burhanuddin-49 <burhanuddin9651@gmail.com>
2021-10-01 21:01:38 +0530

    Basic of JS

---
+++ app.js
@@ -0,0 +1,18 @@
+var a = 2
+var b = 10
+
+console.log('a+b=',a+b)
+console.log('a-b=',a-b)
+console.log('a*b=',a*b)
+console.log('a/b=',a/b)
+console.log('a%b',a%b)
+
+
+// Boolean
+console.warn(a == b);
+console.log(a > b)
+console.log(a < b)
+console.error(a != b)
+
+var f = '10'
+console.log(b + f)
