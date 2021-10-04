d980368157181ae085d2b89b23c8436be7f4e76c
Burhanuddin-49 <burhanuddin9651@gmail.com>
2021-10-05 02:15:53 +0530

    array method

--- app.js
+++ app.js
@@ -1,42 +1,7 @@
-const a = document.querySelectorAll("input")[0];
-const b = document.querySelectorAll("input")[1];
-const add = document.querySelector("#add");
-const subtract = document.querySelector("#subtract");
-const multiply = document.querySelector("#multiply");
-const resultBox = document.querySelector(".result");
+var arr = [1,2,3,4,5,6,7,8]
 
-// const sum = () => {
-//     const result = parseInt(a.value) + parseInt(b.value)
-//     console.log(result)
-//     resultBox.innerHTML = result
-// }
-// const product = () => {
-//     const result = parseInt(a.value) * parseInt(b.value)
-//     console.log(result)
-//     resultBox.innerHTML = result
-// }
+var new_arr = arr.filter((element, index) => {
+    if (element%2==0) return true
+})
 
-const calculate = (event, operation) => {
-  //   if (operation == "add") {
-  //     console.log("adding");
-  //   } else if (operation == "multiply") {
-  //     console.log("multiply");
-  //   }
-  switch (operation) {
-    case "add":
-      resultBox.innerHTML = parseInt(a.value) + parseInt(b.value);
-      break;
-    case "multiply":
-      resultBox.innerHTML = parseInt(a.value) * parseInt(b.value);
-      break;
-    case "subtract":
-      resultBox.innerHTML = parseInt(a.value) - parseInt(b.value);
-      break;
-    default:
-      resultBox.innerHTML = "Not a valid opertion";
-  }
-};
-
-add.addEventListener("click", (event) => calculate(event, "add"));
-multiply.addEventListener("click", (event) => calculate(event, "multiply"));
-subtract.addEventListener("click", (event) => calculate(event,"subtract"))
+console.log(new_arr)
