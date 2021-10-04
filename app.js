0206d85d3efde0c0b16613572d5bf2d93cffdde7
Burhanuddin-49 <burhanuddin9651@gmail.com>
2021-10-05 01:57:10 +0530

    calculator in JS

--- app.js
+++ app.js
@@ -1,8 +1,42 @@
-var body = document.querySelector("body");
-var button = document.querySelector("button");
+const a = document.querySelectorAll("input")[0];
+const b = document.querySelectorAll("input")[1];
+const add = document.querySelector("#add");
+const subtract = document.querySelector("#subtract");
+const multiply = document.querySelector("#multiply");
+const resultBox = document.querySelector(".result");
 
-const callfunc = () => {
-  body.classList.toggle('dark')
+// const sum = () => {
+//     const result = parseInt(a.value) + parseInt(b.value)
+//     console.log(result)
+//     resultBox.innerHTML = result
+// }
+// const product = () => {
+//     const result = parseInt(a.value) * parseInt(b.value)
+//     console.log(result)
+//     resultBox.innerHTML = result
+// }
+
+const calculate = (event, operation) => {
+  //   if (operation == "add") {
+  //     console.log("adding");
+  //   } else if (operation == "multiply") {
+  //     console.log("multiply");
+  //   }
+  switch (operation) {
+    case "add":
+      resultBox.innerHTML = parseInt(a.value) + parseInt(b.value);
+      break;
+    case "multiply":
+      resultBox.innerHTML = parseInt(a.value) * parseInt(b.value);
+      break;
+    case "subtract":
+      resultBox.innerHTML = parseInt(a.value) - parseInt(b.value);
+      break;
+    default:
+      resultBox.innerHTML = "Not a valid opertion";
+  }
 };
 
-button.addEventListener("click", callfunc);
+add.addEventListener("click", (event) => calculate(event, "add"));
+multiply.addEventListener("click", (event) => calculate(event, "multiply"));
+subtract.addEventListener("click", (event) => calculate(event,"subtract"))
