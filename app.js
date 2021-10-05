54bb30a75fd40c627690a354e79d094735e64282
Burhanuddin-49 <burhanuddin9651@gmail.com>
2021-10-06 03:28:04 +0530

    Scope

--- app.js
+++ app.js
@@ -1,29 +1,9 @@
-var button = document.querySelector("button");
-var input = document.querySelector("input");
-var list = document.querySelector("ul");
-
-var chores = [];
-
-var deleteitem = (value) => {
-  const index = chores.indexOf(value);
-  chores.splice(index, 1);
-  console.log(chores);
-};
-
-const callbackfunc = (event) => {
-  const inputvalue = input.value;
-  if (chores.includes(inputvalue)) {
-    console.log("already exits");
-  } else {
-    chores.push(inputvalue);
-    const element = document.createElement("li");
-    const textNode = document.createTextNode(inputvalue);
-    element.appendChild(textNode);
-    list.appendChild(element);
-    element.addEventListener("click", (e) => {
-      e.target.remove();
-    });
+const print = () => {
+  var Name = "Mercedes";
+  if(true){
+      console.log(Name)
   }
+  return Name;
 };
 
-button.addEventListener("click", callbackfunc);
+print()
