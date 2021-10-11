903f6b5c1d3f764bacfd9d522286482fcaf1108a
Burhanuddin-49 <burhanuddin9651@gmail.com>
2021-10-12 02:40:04 +0530

    async

--- app.js
+++ app.js
@@ -1,18 +1,16 @@
-const table = document.querySelector("table");
-console.log(table);
+// fetch("https://jsonplaceholder.typicode.com/users")
+//   .then((resp) => resp.json())
+//   .then((data) => {
+//     console.log(data);
+//     data.forEach((user) => {
+//         console.log(data)
+//     });
+//   });
 
-const reponse = fetch("https://jsonplaceholder.typicode.com/users")
-  .then((resp) => resp.json())
-  .then((data) => {
-    console.log(data);
-    data.forEach((user) => {
-      let newrow = document.createElement("tr");
-      let element = `
-      <th>${user.id}</th>
-      <th>${user.name}</th>
-      <th>${user.email}</th>
-      <th>${user.username}</th>`;
-      newrow.innerHTML = element;
-      table.append(newrow);
-    });
-  });
+const fetchData = async () => {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await response.json();
+  console.log(data);
+};
+
+fetchData()
