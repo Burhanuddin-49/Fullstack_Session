ffec32c9068b7a8a6fc335e0d70b949cde3ee8af
Burhanuddin-49 <burhanuddin9651@gmail.com>
2021-10-12 02:10:39 +0530

    get info from server

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
-let response = fetch("https://jsonplaceholder.typicode.com/users")
-  .then((res) => res.json())
-  .then((book) => {
-    console.log(book);
+const table = document.querySelector("table");
+console.log(table);
+
+const reponse = fetch("https://jsonplaceholder.typicode.com/users")
+  .then((resp) => resp.json())
+  .then((data) => {
+    console.log(data);
+    data.forEach((user) => {
+      let newrow = document.createElement("tr");
+      let element = `
+      <th>${user.id}</th>
+      <th>${user.name}</th>
+      <th>${user.email}</th>
+      <th>${user.username}</th>`;
+      newrow.innerHTML = element;
+      table.append(newrow);
+    });
   });
