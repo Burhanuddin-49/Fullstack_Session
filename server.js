c83ff53376cfed293e7dd59cfaa8929de2e662bd
Burhanuddin-49 <burhanuddin9651@gmail.com>
2021-10-22 01:51:48 +0530

    login&sighup

--- server.js
+++ server.js
@@ -1,16 +1,23 @@
-const http = require('http')
-console.log(http)
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
 
-const server = http.createServer((request,response) =>{
-    
-    // console.log(request.url)
-    // console.log(request.headers)
-    // console.log(request.method)
-    const path = request.url
-    response.write(`You made a request ${path}`)
-    response.end()
-})
+const server = http.createServer((request, response) => {
+  // console.log(request.url)
+  // console.log(request.headers)
+  // console.log(request.method)
+  // const makeup = fs.readFileSync(path.resolve('index.html'))
+  const { url } = request;
+  if (url == "/login") {
+    response.write("<h1>LOGIN</h1>");
+    response.end();
+  }
+  if (url == "/signup") {
+    response.write("<h1>SIGNUP</h1>");
+    response.end();
+  }
+});
 
-server.listen(3000,()=>{
-    console.log(`server listening at PORT: ${3000}`)
-})
+server.listen(3000, () => {
+  console.log(`server listening at PORT: ${3000}`);
+});
