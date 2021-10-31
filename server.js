386234c1d8f135f259899d49c1f634ad7097fe74
Burhanuddin-49 <burhanuddin9651@gmail.com>
2021-11-01 01:43:55 +0530

    auth

--- server.js
+++ server.js
@@ -1,47 +1,18 @@
-// const http = require("http");
-// const fs = require("fs");
-// const path = require("path");
-
-const { request } = require("express");
-
-// const server = http.createServer((request, response) => {
-//   // console.log(request.url)
-//   // console.log(request.headers)
-//   // console.log(request.method)
-//   // const makeup = fs.readFileSync(path.resolve('index.html'))
-//   const { url } = request;
-//   if (url == "/login") {
-//     response.write("<h1>LOGIN</h1>");
-//     response.end();
-//   }
-//   if (url == "/signup") {
-//     response.write("<h1>SIGNUP</h1>");
-//     response.end();
-//   }
-// });
-
-// server.listen(3000, () => {
-//   console.log(`server listening at PORT: ${3000}`);
-// });
-
-const data = [{name: 'bb'},{name: 'bob'}]
-
 const express = require('express')
+const PORT = 3000
+
 const app = express()
 
-app.get('/',(req,res)=>{
-    console.log(req)
-    res.send('This is response')
+const verify = (req,res,next)=>{
+    if (req.headers['user-agent'] === "Thunder Client (https://www.thunderclient.io)") next()
+    else res.send('BLOCKED')
+}
+
+app.get('/',verify,(req, res)=>{
+    res.send("VERIFIED") 
 })
 
-app.get('/login',(req,res)=>{
-    res.send('This is LOGIN')
+app.listen(PORT,()=>{
+    console.log(`server running at port ${PORT}`)
 })
 
-app.get('/getName',(req,res)=>{
-    res.send(data)
-})
-
-app.listen(3000,()=>{
-    console.log("server listening at port 3000")
-})
